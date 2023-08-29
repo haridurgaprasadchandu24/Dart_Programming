ebe4c49d4a531d1a17ec25c6e071df75805e9eaf
haridurgaprasadchandu24 <138667818+haridurgaprasadchandu24@users.noreply.github.com>
2023-08-29 19:32:23 +0530

    Add files via upload

---
+++ String.dart
@@ -0,0 +1,27 @@
+void main() {
+//literals
+  
+  String s3 ="it 's time to code";
+  
+  String s4 ='it \'s time to code';
+  print(s3);
+  print(s4);
+  
+  
+  //interpolation
+  String name ="hello";
+  
+  String age ="24";
+  
+  print(name + age);
+  print("name $age");
+  
+  var l=200;
+  var b=500;
+  String cal="l=$l and b=$b and sum is ${l+b}";
+  print(cal);
+  
+  
+  
+  }
+
